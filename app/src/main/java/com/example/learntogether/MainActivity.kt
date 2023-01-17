0f8c48cf7a84aedbaff2ab49cb2e2a81b81ae387
rafael <k.rafaelalves@gmail.com>
2023-01-17 20:32:47 -0300

    modifer by paramter and weight to 1f

--- app/src/main/java/com/example/learntogether/MainActivity.kt
+++ app/src/main/java/com/example/learntogether/MainActivity.kt
@@ -1,10 +1,8 @@
 package com.example.learntogether
 
-import android.graphics.fonts.FontStyle
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Surface
@@ -13,13 +11,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.learntogether.ui.theme.LearnTogetherTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,67 +24,51 @@ class MainActivity : ComponentActivity() {
         setContent {
             LearnTogetherTheme {
                 Surface {
-                    LearnScreen()
+                    QuadrantScreen()
                 }
             }
         }
     }
 }
 
-
 @Composable
-@Preview(showBackground = true)
-fun ShowTasCompletedIcon() {
-    val image = painterResource(id = R.drawable.ic_task_completed)
-    Image(
-        painter = image,
-        contentDescription = null,
-        modifier = Modifier
-            .fillMaxWidth()
-            .wrapContentHeight(align = Alignment.Top)
-    )
-}
-
-@Composable
-fun LearnScreen() {
+fun QuadrantScreen() {
     Column(
-        modifier = Modifier.fillMaxHeight()
+        modifier = Modifier.fillMaxWidth()
     ) {
         Row(
             modifier = Modifier
-                .fillMaxHeight(0.5f)
-                .fillMaxWidth()
+                .weight(1f)
         ) {
             Tile(
                 title = "Text composable",
                 color = Color.Green,
                 explanation = "Displays text and follows Material Design guidelines.",
-                width = 0.5f
+                modifier = Modifier.weight(1f)
             )
             Tile(
                 title = stringResource(R.string.image_compose),
                 color = Color.Yellow,
                 explanation = stringResource(R.string.image_compose_explanation),
-                width = 1f
+                modifier = Modifier.weight(1f)
             )
         }
 
         Row(
             modifier = Modifier
-                .fillMaxHeight()
-                .fillMaxWidth()
+                .weight(1f)
         ) {
             Tile(
                 title = stringResource(R.string.row_composable),
                 color = Color.Cyan,
                 explanation = stringResource(R.string.row_composable_explanation),
-                width = 0.5f
+                modifier = Modifier.weight(1f)
             )
             Tile(
                 title = stringResource(R.string.column_composable),
                 color = Color.LightGray,
                 explanation = stringResource(R.string.column_composable_explanation),
-                width = 1f
+                modifier = Modifier.weight(1f)
             )
         }
     }
@@ -96,36 +76,26 @@ fun LearnScreen() {
 }
 
 @Composable
-fun Tile(title: String, color: Color, explanation: String, width: Float) {
+fun Tile(title: String, color: Color, explanation: String, modifier: Modifier) {
     Column(
-        modifier = Modifier
+        modifier
+            .fillMaxSize()
             .background(color)
-            .fillMaxWidth(width)
-            .fillMaxHeight()
-            .wrapContentWidth(align = Alignment.CenterHorizontally)
-            .wrapContentHeight(align = Alignment.CenterVertically)
-            .padding(horizontal = 16.dp),
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
         Text(
             text = title,
             fontWeight = FontWeight.Bold,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(bottom = 16.dp)
-                .background(Color.Gray), textAlign = TextAlign.Center
+            modifier = Modifier.padding(bottom = 16.dp)
         )
         Text(
             text = explanation,
-            fontSize = 16.sp,
-            modifier = Modifier
-                .fillMaxWidth(),
             textAlign = TextAlign.Justify,
         )
     }
-
 }
-
-
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
